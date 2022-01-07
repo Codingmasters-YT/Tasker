f3a0bc907b27edf52691273d1e3caaf0ad4943bb
NVirCX <87524238+NVirCX@users.noreply.github.com>
2022-01-07 21:12:50 +0530

    Bruh

--- Final Run/graphics.py
+++ Final Run/graphics.py
@@ -18,8 +18,9 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
         root.title("Tasker")
-        # self.wm_iconbitmap("covid.ico")
-        menubar = tk.Menu(root)
+        root.config(bg="#303443")
+        root.wm_iconbitmap("./Final Run/to-do.ico")
+        menubar = tk.Menu(root, background='#303443', fg='#5fb878')
         navigateMenu = tk.Menu(root, tearoff=0)
         about_us = tk.Menu(root, tearoff=0)
         contact = tk.Menu(root, tearoff=0)
@@ -45,54 +46,63 @@ class App:
 
         root.configure(menu=menubar)
 
+        # dark mode and 
+
         
         InputTitle=tk.Entry(root)
         InputTitle["borderwidth"] = "1px"
         ft = tkFont.Font(family='Roboto',size=14)
         InputTitle["font"] = ft
-        InputTitle["fg"] = "#333333"
+        InputTitle["fg"] = "#5fb878"
+        InputTitle["bg"] = "#3b4152"
         InputTitle["justify"] = "center"
         InputTitle["text"] = "Name For the Task"
-        InputTitle.place(x=10,y=530,width=183,height=58)
+        InputTitle.place(x=10,y=516,width=183,height=58)
         InputTitle.insert(0,"Name For the Task")
 
         InputUrl=tk.Entry(root)
         InputUrl["borderwidth"] = "1px"
         ft = tkFont.Font(family='Roboto',size=14)
         InputUrl["font"] = ft
-        InputUrl["fg"] = "#333333"
+        InputUrl["fg"] = "#5fb878"
+        InputUrl["bg"] = "#3b4152"
         InputUrl["justify"] = "center"
         InputUrl["text"] = "URL of the Website"
-        InputUrl.place(x=200,y=530,width=493,height=58)
+        InputUrl.place(x=200,y=516,width=493,height=58)
         InputUrl.insert(0, "URL for the website")
 
         InputTime=tk.Entry(root)
         InputTime["borderwidth"] = "1px"
         ft = tkFont.Font(family='Roboto',size=14)
         InputTime["font"] = ft
-        InputTime["fg"] = "#333333"
+        InputTime["fg"] = "#5fb878"
+        InputTime["bg"] = "#3b4152"
         InputTime["justify"] = "center"
         InputTime["text"] = "HH/MM/SS"
-        InputTime.place(x=700,y=530,width=133,height=58)
+        InputTime.place(x=700,y=516,width=133,height=58)
         InputTime.insert(0, "HH:MM:SS")
+
         AddBtn=tk.Button(root)
         AddBtn["bg"] = "#5fb878"
         # AddBtn["cursor"] = "Arrow"
         ft = tkFont.Font(family='Roboto',size=14)
         AddBtn["font"] = ft
-        AddBtn["fg"] = "#000000"
+        AddBtn["fg"] = "#303443"
+        AddBtn["activebackground"] = "#5fb88e"
+        AddBtn["activeforeground"] = "#303443"
         AddBtn["justify"] = "center"
         AddBtn["text"] = "Add"
-        AddBtn.place(x=840,y=530,width=149,height=58)
+        AddBtn.place(x=840,y=516,width=149,height=58)
         # AddBtn["command"] = self.AddBtn_command
 
         TitleMsg=tk.Message(root)
         ft = tkFont.Font(family='Roboto',size=29)
         TitleMsg["font"] = ft
-        TitleMsg["fg"] = "#333333"
+        TitleMsg["fg"] = "#5fb88e"
+        TitleMsg["bg"] = "#303443"
         TitleMsg["justify"] = "center"
         TitleMsg["text"] = "Tasks"
-        TitleMsg.place(x=340,y=10,width=289,height=40)
+        TitleMsg.place(x=340,y=10,width=289,height=29)
 
     # def GButton_294_command(self):
     #     print("command")
