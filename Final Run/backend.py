4b4156f39a17ef00ca76b4e635d32807888f531d
Pranay-Chopra <pranaychopra2007@gmail.co>
2022-01-07 14:13:10 +0530

    update backend.py
    Co-authored-by: ThegreatCodert <ThegreatCodert@users.noreply.github.com>
    Co-authored-by: NVirCX <NVirCX@users.noreply.github.com>

--- Final Run/backend.py
+++ Final Run/backend.py
@@ -1,3 +1,35 @@
 import json
-import datetime
 import webbrowser
+import schedule
+import time
+
+global fileformat
+fileformat={
+    "tasks": [
+        
+    ]
+}
+
+def clear_json(filename='Final Run\\task.json'):
+    with open(filename,'w') as file:
+        json.dump(fileformat, file, indent = 4)
+        
+
+while True:
+  task = input("whats do you want to open:")
+  timefortask = input("when do you want to do it: ")
+
+  def tasks():
+    webbrowser.open(task)
+  
+  schedule.every().day.at(timefortask).do(tasks)
+  schedule.every().day.at("00:00").do(clear_json)
+  schedule.run_pending()
+  time.sleep(1)
+
+
+
+
+  # Today : Backend
+  # Tomorrow : Frontend
+  # Day next to tomorrow: Backend and Frontend LINKING
